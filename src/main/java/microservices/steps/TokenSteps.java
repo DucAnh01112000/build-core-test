b2f8b208ba61ae2a07c74269098de4168a5b0201
Sotatek-anhnguyen37 <anh.nguyen37@sotatek.com>
2023-08-22 11:40:54 +0700

    create function verify token id

--- src/main/java/microservices/steps/TokenSteps.java
+++ src/main/java/microservices/steps/TokenSteps.java
@@ -3,11 +3,18 @@ package microservices.steps;
 import constants.Endpoints;
 import core.BaseApi;
 import io.qameta.allure.Step;
+import microservices.models.TokenModel;
+import org.testng.Assert;
 
 public class TokenSteps extends BaseApi {
     @Step("get a token")
     public TokenSteps getToken(String tokenId){
         sendGet(Endpoints.GET_TOKEN, Endpoints.TOKEN_ID, tokenId);
+        return this;
+    }
+    @Step("verify format of token id")
+    public TokenSteps verifyToken(TokenModel tokenModel, String prefix){
+        Assert.assertTrue(tokenModel.getFingerprint().contains(prefix));
         return this;
     }
 }
