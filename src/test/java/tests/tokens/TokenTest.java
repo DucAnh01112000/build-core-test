5733ef633806c7703016e484df99a1f468864eec
Sotatek-anhnguyen37 <anh.nguyen37@sotatek.com>
2023-08-22 18:00:41 +0700

    add test group

--- src/test/java/tests/tokens/TokenTest.java
+++ src/test/java/tests/tokens/TokenTest.java
@@ -11,12 +11,11 @@ public class TokenTest extends BaseTest {
     private TokenSteps tokenSteps = new TokenSteps();
     private String tokenId = "asset17q7r59zlc3dgw0venc80pdv566q6yguw03f0d9";
 
-    @Test(description = "get a token with tokenid")
+    @Test(description = "get a token with tokenid", groups = {"token"})
     public void getToken(){
         TokenModel tokenModel = (TokenModel)
         tokenSteps.getToken(tokenId)
                 .validateResponse(HttpURLConnection.HTTP_OK)
                 .saveResponse(TokenModel.class);
-        System.out.println("fffffff");
     }
 }
