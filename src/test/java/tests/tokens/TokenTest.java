46ac6d5601846e95c344d45748758292fcbad3e4
Sotatek-anhnguyen37 <anh.nguyen37@sotatek.com>
2023-08-25 10:18:21 +0700

    verify  fingerprint of token

--- src/test/java/tests/tokens/TokenTest.java
+++ src/test/java/tests/tokens/TokenTest.java
@@ -11,11 +11,12 @@ public class TokenTest extends BaseTest {
     private TokenSteps tokenSteps = new TokenSteps();
     private String tokenId = "asset17q7r59zlc3dgw0venc80pdv566q6yguw03f0d9";
 
-    @Test(description = "get a token with tokenid", groups = {"token"})
+    @Test(description = "get a token with token id", groups = {"token"})
     public void getToken(){
         TokenModel tokenModel = (TokenModel)
         tokenSteps.getToken(tokenId)
                 .validateResponse(HttpURLConnection.HTTP_OK)
                 .saveResponse(TokenModel.class);
+        tokenSteps.verifyToken(tokenModel, "asset");
     }
 }
